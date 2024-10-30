f21cc42b9b19e0f58b8c934ea9f1f47d75c22f69
iljoonnnn <wkddlfwns99@naver.com>
2024-10-30 17:30:54 +0900

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from PIL import Image
 
-st.write('Hello, *World!* :sunglasses:') # 해당 내용을 수정해서 사이트를 자유롭게 꾸밀 수 있다.
+st.write('나야 장일준 :sunglasses:') # 해당 내용을 수정해서 사이트를 자유롭게 꾸밀 수 있다.
 
 st.title('this is title')
 st.header('this is header')
